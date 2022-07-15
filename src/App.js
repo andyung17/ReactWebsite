df1012008d78101123779849a279f133d5dec556
Andy Ung <andyung123456789@gmail.com>
2022-07-15 19:54:05 -0400

    fixing CI build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
