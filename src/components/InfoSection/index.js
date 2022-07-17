87ba329e5013f9802c9729538098486102697404
Andy Ung <andyung123456789@gmail.com>
2022-07-17 13:05:59 -0400

    Potential information section on webpage

---
+++ src/components/InfoSection/index.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Button } from 'react-scroll'
+import { InfoContainer, 
+    InfoWrapper, 
+    InfoRow, 
+    Column1, 
+    Column2, 
+    TextWrapper, 
+    ImgWrap, 
+    Img, 
+    Topline, 
+    Heading, 
+    Subtitle, 
+    BtnWrap, } from './InfoElements'
+
+const InfoSection = () => {
+  return (
+    <>
+        <InfoContainer>
+            <InfoWrapper>
+                <InfoRow>
+                    <Column1>
+                        <TextWrapper>
+                            <Topline>Topline</Topline>
+                            <Heading>Heading</Heading>
+                            <Subtitle>Subtitle</Subtitle>
+                            <BtnWrap>
+                                <Button to="home"/>
+                            </BtnWrap>
+                        </TextWrapper>
+                    </Column1>
+                    <Column2>
+                        <ImgWrap>
+                            <Img></Img>
+                        </ImgWrap>
+                    </Column2>
+                </InfoRow>
+            </InfoWrapper>
+        </InfoContainer>
+    </>
+  )
+}
+
+export default InfoSection
